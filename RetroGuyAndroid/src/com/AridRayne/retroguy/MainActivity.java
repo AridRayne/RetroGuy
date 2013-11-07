0d2d089c07817370c39f11679b995b54574ec675
AridRayne <aridrayne@gmail.com>
2013-11-07 07:34:53 -0800

    Added themes to the app. Got the main app and settings done. Three themes are available.
    Couldn't find a way to use an integer array without getting null pointer
    exceptions.

--- RetroGuyAndroid/src/com/AridRayne/retroguy/MainActivity.java
+++ RetroGuyAndroid/src/com/AridRayne/retroguy/MainActivity.java
@@ -4,20 +4,36 @@ import org.apache.commons.lang3.StringEscapeUtils;
 
 import roboguice.activity.RoboSherlockFragmentActivity;
 import roboguice.inject.InjectView;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.AridRayne.thegamesdb.lib.Data;
 import com.AridRayne.thegamesdb.lib.PlatformItem;
 import com.AridRayne.thegamesdb.lib.Utilities;
 import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 
 public class MainActivity extends RoboSherlockFragmentActivity {
 
 	@InjectView (R.id.textView1) TextView tv1;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+		String theme = sharedPrefs.getString("appTheme", "default");
+		if (theme.equals("light")) {
+			setTheme(com.roboguice.R.style.Theme_Sherlock_Light);
+		}
+		else if (theme.equals("lightdarkactionbar")) {
+			setTheme(com.roboguice.R.style.Theme_Sherlock_Light_DarkActionBar);
+		}
+		else {
+			setTheme(com.roboguice.R.style.Theme_Sherlock);
+		}
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		new Task().execute(2);
@@ -29,11 +45,21 @@ public class MainActivity extends RoboSherlockFragmentActivity {
 		getSupportMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
 	
-	private class Task extends AsyncTask<Integer, Object, Data> {
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+			return true;
+		}
+		return false;
+	}
 
-		protected Data doInBackground(Integer... params) {
+
+	private class Task extends AsyncTask<Integer, Object, Data<PlatformItem>> {
+
+		protected Data<PlatformItem> doInBackground(Integer... params) {
 			Utilities util = new Utilities();
 			return util.PlatformFromID(2);
 		}
